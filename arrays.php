125d17380e079289126aebac3ca82da3b822ba37
Jezreel John Junio <juniojjv@gmail.com>
2023-04-14 15:44:09 +0800

    Added one color to Arrays.php

--- arrays.php
+++ arrays.php
@@ -1,10 +1,11 @@
 <?php
 
 // $color = array('RED', 'BLUE', 'GREEN');
-
+// Adding one color
 $color[] = 'RED';
 $color[] = 'BLUE';
 $color[] = 'GREEN';
+$color[] = 'YELLOW';
 echo var_dump($color);
 
 
